529c9b2b3e51289c363f487f082e7d66770d97df
Mariana Tapia <mglopez2113@gmail.com>
2022-03-31 20:09:17 -0700

    adding new var and if statements in js

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 // Assignment Code
+var password =""
+var lowercase ="abcdefghijklmnopqrstuvwxyz"
+var uppercase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numbers ="0123456789"
+var specialChars =" !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+var candidates =""
+var length = 0 
+var includeLower = false
+var includeUpper = false
+var includeSpecial = false
+var includeNumber = false
+var letter = ""
+
+
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  var password = "password";
-  // TODO: add code to generate the password here
-
+  function generatePassword() {
+  var length = window.prompt("enter password length:") 
+  if(length <8 , length > 128);
+  alert("password should be from 8 to 128 characters long")
   return password;
-}
-
+ }
+  
+ 
+/*var password = "password";*/
+ 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  /*var password= document.querySelector("#password");*/
 
+  password.value = password;
+
+}
+
+
+includeLower = window.prompt("Would you like to use lowercase letters?:") 
+if (includeLower === true );{
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+password = password + letter
 }
 
 // Add event listener to generate button
