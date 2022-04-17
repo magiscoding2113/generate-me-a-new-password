586ef55215651b345071b02d1c49fe4f5d39630c
Mariana Tapia <mglopez2113@gmail.com>
2022-04-16 21:14:11 -0700

    updated js

--- script.js
+++ script.js
@@ -1,48 +1,93 @@
 // Assignment Code
-var password ="";
+var passwordSelected;
 var lowercase ="abcdefghijklmnopqrstuvwxyz";
 var uppercase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers ="0123456789";
 var specialChars =" !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 var candidates ="l M ! 6";
 var length;
-//var includeLower //confirm ("would you like to use lowercase letters?;");
+var includeLower = false;
 var includeUpper = false;
 var includeSpecial = false;
 var includeNumber = false;
 var letter = "";
-includeLower = "Would you like to use lowercase letters?:"
+var selectedCharacters = [];
+
+
+
 function generateRandom(string) {
-Math.random()*string.length
-console.log(string);
+return Math.floor(Math.random()*string.length);
 }
-generateRandom(lowercase);
+
+
+
+
+
+
 var generateBtn = document.querySelector("#generate");
 
-  function generatePassword() {
-  var length = prompt("Enter password length:"); 
-  includeLower= confirm("would you like to use lowercase letters?:")
-  //includeUpper = confirm("would you like to use Uppercase letters?:");
-  //includeSpecial = confirm("would you like to use a Special character?:");
-  //includeNumber = confirm("would you like to use a Number?:");
+function generatePassword() {
+  do {
+    var length = prompt("Please select a password, length must be between 8 and 128 characters:")
+  } while (length < 8 || length > 128);
+  includeLower = confirm("Would you like to use Lowercase letters?:");
+  includeUpper = confirm("would you like to use Uppercase letters?:");
+  includeSpecial = confirm("would you like to use a Special character?:");
+  includeNumber = confirm("would you like to use a Number?:");
   
-if (length < 8) {
-alert("Password length must be at least 8 characters");
-}
-if (length > 128 ) {
-  alert ("Password length must be under 128 characters");
-}
-if (includeLower === true ) {
- generateRandom(includeLower);
-}
-//if (includeUpper != true) {
-  //alert(includeSpecial);
- //} 
- //else if (includeUpper === true) {
-    //generateRandom(uppercase);
-  //}
+  if(
+    includeLower === false &&
+    includeUpper === false &&
+    includeSpecial === false &&
+    includeNumber === false) {
+      alert("You must select at least one character type");
+      return false;
+    }
+ 
 
+
+  if (includeLower === true ) {
+   //var lowercase = slice(generateRandom);
+  lowercase = lowercase.slice(lowercase);
+  selectedCharacters.push(generateRandom(lowercase));
+   //console.log(generateRandom(lowercase));
+   //console.log(lowercase.slice(4,20));
+  }
+  if (includeUpper === true) {
+   uppercase = uppercase.slice(uppercase);
+   selectedCharacters.push(generateRandom(uppercase)); 
+   //console.log(generateRandom(uppercase));
+   //console.log(uppercase.slice(10,23));
+  }
+  if (includeSpecial === true) {
+    specialChars = specialChars.slice(specialChars);
+    selectedCharacters.push(generateRandom(specialChars));
+    //console.log(generateRandom(specialChars));
+    //console.log(specialChars.slice(1,8));
+  }
+  if (includeNumber === true) {
+    //numbers = numbers.slice(numbers);
+    selectedCharacters.push(generateRandom(numbers));
+    //console.log(generateRandom(numbers));
+  }
+  
+  //for (var i = 0; i 
 }
+
+
+
+
+
+//var passwordSelected = {
+  //length: length,
+  //specialChars: specialChars,
+  //numbers: numbers,
+  //lowercase: lowercase,
+  //uppercase: uppercase,
+//};
+
+//eturn passwordSelected;
+//}
 
 function writePassword() {
 var password = generatePassword("#password");
